d184b5ed4eb08986665d5b443286a10be33c6587
brbiancr <brbiancr@gmail.com>
2025-10-15 16:21:38 -0300

    script de testes locais para limpeza e padronização dos csvs

---
+++ Etapa-2/local/script_local_csv.py
@@ -0,0 +1,59 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, split, explode
+from dotenv import load_dotenv
+
+#PATHS
+load_dotenv()
+path_movies = os.getenv("path_movies")
+path_series = os.getenv("path_series")
+teste = os.getenv("teste")
+
+spark = SparkSession \
+    .builder \
+    .master("local[*]") \
+    .appName("job2_etl") \
+    .getOrCreate()
+
+# ===========
+# CSV movies
+# ===========
+
+df_movies = spark.read.csv(path_movies, header=True, inferSchema=True, sep="|")
+
+df_movies = (df_movies
+            .withColumn("anoLancamento", col("anoLancamento").cast("int"))
+            .withColumn("tempoMinutos", col("tempoMinutos").cast("int"))
+            .withColumn("anoNascimento", col("anoNascimento").cast("int"))
+            .withColumn("anoFalecimento", col("anoFalecimento").cast("int"))
+            .withColumnRenamed("tituloPincipal", "tituloPrincipal")
+)
+
+df_movies = (df_movies
+            .withColumn("genero", explode(split(col("genero"), r",\s*")))
+            .withColumn("generoArtista", explode(split(col("generoArtista"), r",\s*")))
+            .withColumn("profissao", explode(split(col("profissao"), r",\s*")))
+            .withColumn("titulosMaisConhecidos", explode(split(col("titulosMaisConhecidos"), r",\s*")))
+)
+
+# ===========
+# CSV series
+# ===========
+
+df_series = spark.read.csv(path_series, header=True, inferSchema=True, sep="|")
+
+df_series = (df_series
+            .withColumn("anoLancamento", col("anoLancamento").cast("int"))
+            .withColumn("anoTermino", col("anoTermino").cast("int"))
+            .withColumn("tempoMinutos", col("tempoMinutos").cast("int"))
+            .withColumn("anoNascimento", col("anoNascimento").cast("int"))
+            .withColumn("anoFalecimento", col("anoFalecimento").cast("int"))
+            .withColumnRenamed("tituloPincipal", "tituloPrincipal")
+)
+
+df_series = (
+    df_series
+    .withColumn("genero", explode(split(col("genero"), r",\s*")))
+    .withColumn("profissao", explode(split(col("profissao"), r",\s*")))
+    .withColumn("titulosMaisConhecidos", explode(split(col("titulosMaisConhecidos"), r",\s*")))
+)
